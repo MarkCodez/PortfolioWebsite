a4424a8e6773ede5f1431f83ce1558659fa998e3
MarkCodez <markeskander745@gmail.com>
2023-11-17 13:40:34 -0600

    new navbar with buttons and cool styles.

---
+++ index.js
@@ -0,0 +1,15 @@
+function navigateToHome() {
+    window.location.href = '#section1';
+}
+function navigateToTimeline() {
+    window.location.href = '#section2';
+}
+function navigateToProjects() {
+    window.location.href = '#section3';
+}
+function navigateToSkills() {
+    window.location.href = '#section4';
+}
+function navigateToContacts() {
+    window.location.href = '#section5';
+}
